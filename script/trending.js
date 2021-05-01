9bb86ef15a41230a8c5ef4794bd84846abbf2ad1
JuanKmilo18 <juancamilobuitrago2003@gmail.com>
2021-05-01 13:44:23 -0500

    Updated trends

--- script/trending.js
+++ script/trending.js
@@ -17,4 +17,29 @@ flechaIzquierda.addEventListener('click', () => {
 // gifs de giphy
 
 const apiKey = 'n8KtgV7bEMIp74WJ2vJjRcoZXAvQiPX5';
-const path = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=15`;
+const limit = 20;
+const path = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=${limit}`;
+const carousel = document.querySelector(".carousel");
+
+async function getData (url) {
+  const response = await fetch(url);
+  const json = await response.json();
+
+  json.data.forEach((item)=>{
+    const gif_container = document.createElement("div");
+    gif_container.classname = "gif";
+    carousel.appendChild(gif_container);
+
+    const link = document.createElement("a");
+    link.classname = "gifi-link";
+    gif_container.appendChild(link);
+
+    const img = document.createElement("img");
+    img.classname = "gifi";
+    img.src = item.images.original.url;
+    img.alt = item.title;
+    link.appendChild(img);
+  })
+};
+
+getData(path);
