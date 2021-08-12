33f7ecc8e320dcc4014b326b8e144e995ddbbf8b
JuanKmilo18 <juancamilobuitrago2003@gmail.com>
2021-08-12 11:39:54 -0500

    Hecho trending, crearGifo y misGifos.

--- script/trending.js
+++ script/trending.js
@@ -1,159 +1,203 @@
-//trending
+const $sliderContainer = document.querySelector(".slider__container");
 
-const fila = document.querySelector('.content-car');
-const gifs = document.querySelectorAll('.gif');
+const API_KEY = "n8KtgV7bEMIp74WJ2vJjRcoZXAvQiPX5";
+const MAXIMUM_GIFS = 20;
+const TRENDINGS_ENDPOINT = `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=${MAXIMUM_GIFS}`;
 
-const flechaIzquierda = document.getElementById('left-arrow');
-const flechaDerecha = document.getElementById('right-arrow');
+const favoritesArray = [];
 
-flechaDerecha.addEventListener('click', () => {
-  fila.scrollLeft += fila.offsetWidth;
-});
+const addToTheFavoritesSection = () => {
+  if (localStorage.getItem("someFavorites")) {
+    const alreadySavedItems = JSON.parse(localStorage.getItem("someFavorites"));
+    favoritesArray.push(...alreadySavedItems);
+  }
+  const itemsMap2 = favoritesArray.map((item) => [item.id, item]);
+  const itemsMapArr2 = new Map(itemsMap2);
 
-flechaIzquierda.addEventListener('click', () => {
-  fila.scrollLeft -= fila.offsetWidth;
-});
+  const uniques = [...itemsMapArr2.values()];
+  localStorage.setItem("someFavorites", JSON.stringify(uniques));
+};
 
-// gifs de giphy
+const $header = document.querySelector("#header");
+$header.addEventListener("click", addToTheFavoritesSection);
 
-const apiKey = 'n8KtgV7bEMIp74WJ2vJjRcoZXAvQiPX5';
-const limit = 20;
-const path = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=${limit}`;
-const carousel = document.querySelector(".carousel")
+async function getTrendings(url) {
+  const resp = await fetch(url);
+  const json = await resp.json();
 
-async function getData (url) {
-  const response = await fetch(url);
-  const json = await response.json();
+  json.data.forEach((element) => {
+    const $sliderItem = document.createElement("div");
+    $sliderItem.className = "slider__item";
+    $sliderContainer.appendChild($sliderItem);
 
-  json.data.forEach((item)=>{
-    const gif_container = document.createElement("div");
-    gif_container.className = "gif";
-    carousel.appendChild(gif_container);
+    const $gif = document.createElement("img");
+    $gif.className = "slider__img";
+    $gif.src = element.images.original.url;
+    $gif.alt = element.title;
+    $sliderItem.appendChild($gif);
 
-    const link = document.createElement("a");
-    link.className = "gifi-link";
-    gif_container.appendChild(link);
+    const $sliderHover = document.createElement("div");
+    $sliderHover.className = "slider__hover";
+    $sliderItem.appendChild($sliderHover);
 
-    const img = document.createElement("img");
-    img.className = "gifi";
-    img.src = item.images.original.url;
-    img.alt = item.title;
-    link.appendChild(img);
+    const $sliderIcons = document.createElement("div");
+    $sliderIcons.className = "slider__icons";
+    $sliderItem.appendChild($sliderIcons);
 
-    const hover_img = document.createElement("div");
-    hover_img.className = "hover-img";
-    hover_img.id = "hover-img";
-    link.appendChild(hover_img);
+    const $firstIconContainer = document.createElement("div");
+    $firstIconContainer.className = "slider__icon-container";
+    $sliderIcons.appendChild($firstIconContainer);
+    const $firstIcon = document.createElement("span");
+    $firstIcon.className = "slider__icon--first";
+    $firstIconContainer.appendChild($firstIcon);
 
-// Aquí comienza la parte de lo del corazón, descarga y hacerle zoom/click
+    const $secondIconContainer = document.createElement("div");
+    $secondIconContainer.className = "slider__icon-container";
+    $sliderIcons.appendChild($secondIconContainer);
+    const $secondIcon = document.createElement("span");
+    $secondIcon.className = "slider__icon--second";
+    $secondIconContainer.appendChild($secondIcon);
 
-    const content_fav = document.createElement("div");
-    content_fav.className = "content-fav";
-    hover_img.appendChild(content_fav);
+    const downloadGif = async () => {
+      const myGif = await fetch(`https://media.giphy.com/media/${element.id}/giphy.gif`);
+      const file = await myGif.blob();
+      const urlBlob = URL.createObjectURL(file);
+      const $aTag = document.createElement("a");
+      $aTag.download = `${element.title}.gif`;
+      $aTag.href = urlBlob;
+      $aTag.click();
+    };
 
-    const heart_box = document.createElement("div");
-    heart_box.className = "heart-box";
-    content_fav.appendChild(heart_box);
+    $secondIconContainer.addEventListener("click", () => {
+      downloadGif();
+    });
 
-    const label = document.createElement("label");
-    heart_box.appendChild(label);
+    const $lastIconContainer = document.createElement("div");
+    $lastIconContainer.className = "slider__icon-container";
+    $sliderIcons.appendChild($lastIconContainer);
+    const $lastIcon = document.createElement("span");
+    $lastIcon.className = "slider__icon--last";
+    $lastIconContainer.appendChild($lastIcon);
 
-    const check = document.createElement("input");
-    check.type = "checkbox";
-    check.id = "check";
-    check.value = "heart_1";
-    label.appendChild(check);
+    const $sliderText = document.createElement("div");
+    $sliderText.className = "slider__text";
+    $sliderItem.appendChild($sliderText);
 
-    const img_heart = document.createElement("img");
-    img_heart.className = "img-heart";
-    img_heart.src = "./assets/icon-fav.svg";
-    img_heart.alt = "Heart";
-    label.appendChild(img_heart);
+    const $sliderParagraph = document.createElement("p");
+    $sliderParagraph.className = "slider__p";
+    if (element.username === "") {
+      $sliderParagraph.textContent = "Desconocido";
+    } else {
+      $sliderParagraph.textContent = element.username;
+    }
+    $sliderText.appendChild($sliderParagraph);
 
-    const bx_act = document.createElement("div");
-    bx_act.className = "bx-act";
-    label.appendChild(bx_act);
+    const $sliderTitle = document.createElement("p");
+    $sliderTitle.className = "slider__title";
+    $sliderTitle.textContent = element.title;
+    $sliderText.appendChild($sliderTitle);
 
-    const img_heart_act = document.createElement("img");
-    img_heart_act.className = "img-heart-act";
-    img_heart_act.src = "./assets/icon-fav-active.svg";
-    img_heart_act.alt = "Heart Active";
-    bx_act.appendChild(img_heart_act);
+    function openFullscreen() {
+      if ($sliderItem.requestFullscreen) {
+        $sliderItem.requestFullscreen();
+      } else if ($sliderItem.webkitRequestFullscreen) {
+        $sliderItem.webkitRequestFullscreen();
+      } else if ($sliderItem.msRequestFullscreen) {
+        $sliderItem.msRequestFullscreen();
+      }
+    }
 
-    const img_heart_hov = document.createElement("img");
-    img_heart_hov.className = "img-heart-hov";
-    img_heart_hov.src = "./assets/icon-fav-hover.svg";
-    img_heart_hov.alt = "Heart Active";
-    label.appendChild(img_heart_hov);
+    function closeFullscreen() {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      }
+    }
 
-    // Aquí empieza la parte de download
+    const makeAGifFullScreen = () => {
+      $sliderItem.id = "mainDiv";
+      $gif.id = "theGif--";
+      $sliderHover.id = "theHoverDiv--";
+      $sliderIcons.id = "sliderICons--";
+      $firstIconContainer.id = "iconsContainers1--";
+      $secondIconContainer.id = "iconsContainers2--";
+      $lastIconContainer.id = "iconsContainers3--";
+      $firstIcon.id = "firstIcon--";
+      $secondIcon.id = "secondIcon--";
+      $lastIcon.id = "lastIcon--";
+      $sliderText.id = "textContainer--";
+      $sliderParagraph.id = "textUser--";
+      $sliderTitle.id = "textTitle--";
 
-    const down_box = document.createElement("div");
-    down_box.className = "down-box";
-    content_fav.appendChild(down_box);
+      openFullscreen();
 
-    const label_down = document.createElement("label");
-    down_box.appendChild(label_down);
+      $lastIconContainer.removeEventListener("click", makeAGifFullScreen);
 
-    const check_down = document.createElement("input");
-    check_down.type = "checkbox";
-    check_down.id = "checkDown";
-    check_down.value = "down_1";
-    label_down.appendChild(check_down);
+      $lastIconContainer.addEventListener("click", whenCloseFullScreen);
+    };
 
-    const img_down = document.createElement("img");
-    img_down.className = "img-down";
-    img_down.src = "./assets/icon-download.svg";
-    img_down.alt = "Download";
-    label_down.appendChild(img_down);
+    const whenCloseFullScreen = () => {
+      $sliderItem.removeAttribute("id");
+      $gif.removeAttribute("id");
+      $sliderHover.removeAttribute("id");
+      $sliderIcons.removeAttribute("id");
+      $firstIconContainer.removeAttribute("id");
+      $secondIconContainer.removeAttribute("id");
+      $lastIconContainer.removeAttribute("id");
+      $firstIcon.removeAttribute("id");
+      $secondIcon.removeAttribute("id");
+      $lastIcon.removeAttribute("id");
+      $sliderText.removeAttribute("id");
+      $sliderParagraph.removeAttribute("id");
+      $sliderTitle.removeAttribute("id");
 
-    const img_down_hov = document.createElement("img");
-    img_down_hov.className = "img-down-hov";
-    img_down_hov.src = "./assets/icon-download-hover.svg";
-    img_down_hov.alt = "Download hover";
-    label_down.appendChild(img_down_hov);
+      closeFullscreen();
 
-    // Aquí termina la parte de download
+      $lastIconContainer.removeEventListener("click", whenCloseFullScreen);
 
-    // Parte hacerle zoom
+      $lastIconContainer.addEventListener("click", makeAGifFullScreen);
+    };
 
-    const zoom_box = document.createElement("div");
-    zoom_box.className = "zoom-box";
-    content_fav.appendChild(zoom_box);
+    $lastIconContainer.addEventListener("click", makeAGifFullScreen);
 
-    const label_zoom = document.createElement("label");
-    zoom_box.appendChild(label_zoom);
+    const addToFavorites = () => {
+      $firstIcon.style = `background-image: url("./assets/icon-heart-full.svg");`;
 
-    const check_zoom = document.createElement("input");
-    check_zoom.type = "checkbox";
-    check_zoom.id = "checkZoom";
-    check_zoom.value = "zoom_1";
-    label_zoom.appendChild(check_zoom);
+      favoritesArray.push(element);
 
-    const img_zoom = document.createElement("img");
-    img_zoom.className = "img-zoom";
-    img_zoom.src = "./assets/icon-max-normal.svg";
-    img_zoom.alt = "Max";
-    label_zoom.appendChild(img_zoom);
+      $firstIconContainer.removeEventListener("click", addToFavorites);
+      $firstIconContainer.addEventListener("click", removeFromFavorites);
+    };
 
-    const img_zoom_hov = document.createElement("img");
-    img_zoom_hov.className = "img-zoom-hov";
-    img_zoom_hov.src = "./assets/icon-max-hover.svg";
-    img_zoom_hov.alt = "Zoom hover";
-    label_zoom.appendChild(img_zoom_hov);
+    const removeFromFavorites = () => {
+      $firstIcon.style = `background-image: url("./assets/icon-heart.svg");`;
 
-    // Finaliza parte hacerle zoom
+      if (favoritesArray.includes(element)) {
+        favoritesArray.pop(element);
+      }
 
-// Aquí termina todo eso
+      $firstIconContainer.removeEventListener("click", removeFromFavorites);
+      $firstIconContainer.addEventListener("click", addToFavorites);
+    };
 
-    const subtitle = document.createElement("h2");
-    subtitle.className = "subtitle";
-    hover_img.appendChild(subtitle);
-
-    const title = document.createElement("h1");
-    title.className = "title-gif";
-    hover_img.appendChild(title);
-  })
+    $firstIconContainer.addEventListener("click", addToFavorites);
+  });
 }
 
-getData(path);
+getTrendings(TRENDINGS_ENDPOINT);
+
+// Slider
+
+const $leftArrow = document.querySelector(".slider__arrow");
+const $rightArrow = document.querySelectorAll(".slider__arrow")[1];
+
+$leftArrow.addEventListener("click", () => {
+  $sliderContainer.scrollLeft -= 400;
+});
+
+$rightArrow.addEventListener("click", () => {
+  $sliderContainer.scrollLeft += 400;
+});
